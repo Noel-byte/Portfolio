3844acf602db4164207f02b0a5ee40c69e4b4490
Noel <noeltesdev@gmail.com>
2025-03-24 14:34:52 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <>
-        <p><span>&copy; right</span> This is a footer</p>
+        <p><span>&copy;2025 by Noel Tesfe</span> This is a footer</p>
         </>
     )
 }
