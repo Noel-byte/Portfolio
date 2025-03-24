9a39691606f9f4a44d4667602932b1cc98fdcc0d
Noel <noeltesdev@gmail.com>
2025-03-24 14:10:12 -0600

    added a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import Header from "./components/header"
 function App() {
 
 
   return (
     <>
-         <h1 className=' text-blue-600 text-3xl text-center mt-3'>Welcome to the site</h1>
-         <h2>Personal details</h2>
-          <h3 className=" text-center bg-stone-400 text-stone-50">footer</h3>
+         <Header/>
     </>
   )
 }
