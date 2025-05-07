23c0ad8785bc3344f72bb8736bbca9a49b36ead9
Noel <noeltesdev@gmail.com>
2025-05-07 16:22:40 -0600

    modified css

--- src/components/CheckOutWork.jsx
+++ src/components/CheckOutWork.jsx
@@ -5,7 +5,7 @@ export const CheckOutWork = ({handleClick,label}) => {
 
   return (
    <button onClick={handleClick} 
-   className='rounded bg-stone-50 px-8 py-3 w-100 block m-auto mt-6 hover:cursor-pointer hover:bg-backcolor hover:text-stone-50 font-buttons'>
+   className='rounded bg-stone-200 px-8 text-lg py-3 sm:w-1/2 block m-auto mt-6 hover:cursor-pointer hover:bg-dark hover:text-stone-50 font-buttons'>
     {label}
     </button>
   )
