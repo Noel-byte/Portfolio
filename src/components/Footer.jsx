52025a17d8f2725935b1ed8d376c26045fe0b89e
Noel <noeltesdev@gmail.com>
2025-05-07 10:53:51 -0600

    modified Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
-export default function Footer(){
-    return(
-        <>
-        <p className="absolute  left-1/2 -translate-x-1/2 sm:text-sm text-gray-500">&copy;2025 by Noel Tesfe All rights reserved.</p>
-        </>
-    )
+export default function Footer() {
+  return (
+    <>
+      <p className="sm:text-sm text-gray-500 text-center py-2">
+        &copy;2025 by Noel Tesfe All rights reserved.
+      </p>
+    </>
+  );
 }
